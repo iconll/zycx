7f69cd760063bea65d635660f05c20085464d645
iconll <872840601@qq.com>
2019-08-01 14:32:29 +0800

    的

--- zycx/src/main/java/com/zycx/system/common/config/security/MyAuthenctiationFailureHandler.java
+++ zycx/src/main/java/com/zycx/system/common/config/security/MyAuthenctiationFailureHandler.java
@@ -1,5 +1,6 @@
 package com.zycx.system.common.config.security;
 
+import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,7 +14,9 @@ import org.springframework.stereotype.Component;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * 自定义登录失败处理
@@ -25,16 +28,24 @@ import java.io.IOException;
 public class MyAuthenctiationFailureHandler implements AuthenticationFailureHandler {
 
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-     @Autowired
-    private ObjectMapper objectMapper;
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException,UsernameNotFoundException {
 
-          //有提示，不跳转
-//         response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-//         response.setContentType("application/json;charset=utf-8");
-//         response.getWriter().write(objectMapper.writeValueAsString(e.getMessage()));
-           //无提示直接跳转
-         redirectStrategy.sendRedirect(request, response, "/login?error=true");
+        String mst=request.getParameter("type");
+        if(mst!=null && mst.equals("move")){
+            //移动端登录成功后跳转的地址
+            JSONObject json =new JSONObject();
+            json.put("resultMsg","fail");
+            PrintWriter out = response.getWriter();
+            out.write(json.toJSONString());
+            out.flush();
+            out.close();
+        }else{
+        HttpSession session = request.getSession();
+        session.setAttribute("errorMsg",e.getMessage());
+        session.setAttribute("error",true);
+        redirectStrategy.sendRedirect(request, response, "/login");
         }
+    }
 }
